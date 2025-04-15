87d43e25325d7fa0d7fb120471e969ba9ca8150e
Jahnavi7143 <158245536+Jahnavi7143@users.noreply.github.com>
2025-04-16 02:02:08 +0530

    Changed UI for profile page

--- frontend/src/components/User/UserLocationCard.jsx
+++ frontend/src/components/User/UserLocationCard.jsx
@@ -10,18 +10,19 @@ const UserLocationCard = ({ id, image, name,role }) => {
     }
   }
   return (
-    <div className="bg-white rounded-lg shadow-md p-4 w-64 h-64 flex flex-col items-center justify-between">
+    <div className="bg-white rounded-lg shadow-md p-4 w-64 h-64 flex flex-col items-center justify-between location-card">
       {/* Clickable Image */}
       <img
         src={image}
         alt={name}
-        className="w-full h-36 object-cover rounded-lg cursor-pointer"
+        className="w-full h-40 object-cover rounded-lg cursor-pointer"
         onClick={HandleNavigate}
       />
 
       {/* Clickable Location Name */}
       <h2
-        className="text-lg font-semibold mt-2 cursor-pointer hover:text-blue-600"
+        className="text-lg font-semibold mt-2 mb-2 cursor-pointer hover:text-blue-600"
+        style={{fontSize:"25px"}}
         onClick={HandleNavigate}
       >
         {name}
